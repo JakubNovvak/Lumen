421aa4d8037d446122fadb3d9a9548ebbe0f47cf
Jakub Nowak <jakubszymonnowak@gmail.com>
2023-11-04 12:37:05 +0100

    Change default page's content

--- Source/Web/Lumen.Web.GUI/lumen.web.gui/src/App.tsx
+++ Source/Web/Lumen.Web.GUI/lumen.web.gui/src/App.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+﻿import React from 'react';
+import { styled, alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 import logo from './assets/lightbulb-placeholder.png';
 import './App.css';
 
+const Container = styled(Box)(({ theme }) => ({
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center"
+}));
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo}/>
-        <p>
-          It would be nice, to change something here...
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Container>
+                    <img src={logo} style={{ maxHeight: "80px" }} />
+                    <h1>Lumen</h1><h5>©</h5>
+                </Container>
+            </header>
+        </div>
+    );
 }
 
 export default App;

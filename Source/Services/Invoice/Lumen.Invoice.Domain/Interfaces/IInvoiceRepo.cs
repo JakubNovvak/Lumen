aa16e34690f564f268d98c59112a135293260502
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-10 19:35:03 +0100

    Prepare repository interface

---
+++ Source/Services/Invoice/Lumen.Invoice.Domain/Interfaces/IInvoiceRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Lumen.Invoice.Domain.Common;
+using Lumen.Invoice.Domain.Entities;
+using Lumen.Invoice.Domain.Entities.Mains;
+
+namespace Lumen.Invoice.Domain.Interfaces
+{
+    public interface IInvoiceRepo
+    {
+        bool SaveChanges();
+        IEnumerable<ElectricityInvoice> GetAllElectricityInvoices();
+        ElectricityInvoice GetElectricityInvoice(int id);
+        string GetNameOfAdministrativeUnit(int id);
+    }
+}

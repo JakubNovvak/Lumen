dd1524a187813ae47cca72e67974b824dc2dcd87
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-11 00:27:25 +0100

    Add another test Action in the controller

--- Source/Services/Invoice/Lumen.Invoice.API/Controllers/InvoiceController.cs
+++ Source/Services/Invoice/Lumen.Invoice.API/Controllers/InvoiceController.cs
@@ -1,4 +1,6 @@
-﻿using Lumen.Invoice.Domain.Entities.Mains;
+﻿using Lumen.Invoice.Application.Dtos;
+using Lumen.Invoice.Application.Services;
+using Lumen.Invoice.Domain.Entities.Mains;
 using Lumen.Invoice.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +10,42 @@ namespace Lumen.Invoice.API.Controllers
     [Route("api/[controller]")]
     public class InvoiceController : ControllerBase
     {
-        public InvoiceController()
-        {
+        private readonly IInvoicesService _invoicesService;
 
+        public InvoiceController(IInvoicesService invoicesService)
+        {
+            _invoicesService = invoicesService;
         }
 
         [HttpGet("/tests/HelloWorld", Name = "HelloWorldAction")]
         public ActionResult TestController()
         {
             return Ok("Hello World!");
+        }
+
+        [HttpGet("/tests/ElectricityInvoices", Name = "ElectricityInvoicesTest")]
+        public ActionResult<IEnumerable<ElectricityInvoiceReadDto>> GetAllElectricityInvoices()
+        {
+            try
+            {
+                var electricityInvoicesReadDto = _invoicesService.GetAllElectricityInvoices();
+                return Ok(electricityInvoicesReadDto);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($">[TestsCtrl] There was an error while retrieving the electricity invoices: {ex.Message}");
+                return BadRequest($"There was an error while retrieving the electricity invoices: {ex.Message}");
+            }
+            catch (KeyNotFoundException ex)
+            {
+                Console.WriteLine($">[TestsCtrl] There is no electricity invoices in the database: {ex.Message}");
+                return BadRequest($"There is no electricity invoices in the database: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($">[TestsCtrl] There was an unexpected error: {ex.Message}");
+                return BadRequest($"There was an unexpected error: {ex.Message}");
+            }
         }
     }
 }

fabe4a4d45d5e4386e4a081f4a1c3295c6adf82d
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-11 00:26:11 +0100

    Create basic dtos with corresponding profile

---
+++ Source/Services/Invoice/Lumen.Invoice.Application/Dtos/EntityBaseCreateDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lumen.Invoice.Application.Dtos
+{
+    public class EntityBaseCreateDto
+    {
+        public string? InvoiceNumber { get; set; }
+        public DateTime InitialBillingDate { get; set; }
+        public DateTime FinalBillingDate { get; set; }
+        public int Status { get; set; }
+        public bool EReaderSubmitted { get; set; }
+        public int /*TODO: User*/ WhoSubmitted { get; set; }
+        public int /*TODO: User*/ WhoApproved { get; set; }
+        public DateTime WhenSubmitted { get; set; }
+        public DateTime WhenLastModified { get; set; }
+        public string? SubmitterComment { get; set; }
+        public string? ApproverComment { get; set; }
+        public int administrativeUnitId { get; set; }
+    }
+}
---
+++ Source/Services/Invoice/Lumen.Invoice.Application/Profiles/InvoicesProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Lumen.Invoice.Application.Dtos;
+using Lumen.Invoice.Domain.Common;
+using Lumen.Invoice.Domain.Entities.Mains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lumen.Invoice.Application.Profiles
+{
+    public class InvoicesProfile: Profile
+    {
+        public InvoicesProfile()
+        {
+            //BaseEntity Maps
+            CreateMap<EntityBase, EntityBaseReadDto>();
+            CreateMap<EntityBaseCreateDto, EntityBase>();
+
+            //ElectricityInvoice Maps
+            CreateMap<ElectricityInvoice, ElectricityInvoiceReadDto>()
+                .IncludeBase<EntityBase, EntityBaseReadDto>();
+
+            CreateMap<ElectricityInvoiceCreateDto, ElectricityInvoice>()
+                .IncludeBase<EntityBaseCreateDto, EntityBase>();
+
+            //TODO: Maps of all other Invoices
+        }
+    }
+}

04a54aaadf5727ce91f28bcc6d8d34674fe40a74
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-10 19:36:47 +0100

    Implement new Controller with a placeholder action

---
+++ Source/Services/Invoice/Lumen.Invoice.API/Controllers/InvoiceController.cs
@@ -0,0 +1,22 @@
+﻿using Lumen.Invoice.Domain.Entities.Mains;
+using Lumen.Invoice.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lumen.Invoice.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InvoiceController : ControllerBase
+    {
+        public InvoiceController()
+        {
+
+        }
+
+        [HttpGet("/tests/HelloWorld", Name = "HelloWorldAction")]
+        public ActionResult TestController()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}

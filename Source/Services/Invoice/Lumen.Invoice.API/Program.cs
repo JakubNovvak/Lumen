b136d930e4049747d71f48de5827a70844aeb853
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-10 23:08:49 +0100

    Small code cleanup

--- Source/Services/Invoice/Lumen.Invoice.API/Program.cs
+++ Source/Services/Invoice/Lumen.Invoice.API/Program.cs
@@ -10,7 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 #region Services Configuration
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
--- Source/Services/Invoice/Lumen.Invoice.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Lumen.Invoice.Application
-{
-    public class Class1
-    {
-
-    }
-}

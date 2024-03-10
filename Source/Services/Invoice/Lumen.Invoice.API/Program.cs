231093af473c798c98fddcd82cf78be9ec28e735
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-10 19:42:02 +0100

    Add a Program.cs sections with "region" and add In Memory database initialization

--- Source/Services/Invoice/Lumen.Invoice.API/Program.cs
+++ Source/Services/Invoice/Lumen.Invoice.API/Program.cs
@@ -1,15 +1,42 @@
+using Lumen.Invoice.Domain.Interfaces;
+using Lumen.Invoice.Infrastructure.Presistence;
+using Lumen.Invoice.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+#region Services Configuration
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+if (builder.Environment.IsDevelopment())
+{
+    Console.WriteLine(">[EnvMode] Starting Application in Development Mode.");
+    Console.WriteLine(">[DBConf] Using In Memory Database.");
+
+    builder.Services.AddDbContext<InvoiceDbContext>(opt =>
+        opt.UseInMemoryDatabase("InMem"));
+}
+else
+{
+    //TODO: SQL Server for Production
+}
+
+builder.Services.AddScoped<IInvoiceRepo, InvoiceRepo>();
+
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+#region App Configuration
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -20,6 +47,10 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+InvoiceDbContextSeed.PrepPopulation(app, builder.Environment.IsProduction());
+
 app.MapControllers();
 
 app.Run();
+
+#endregion
